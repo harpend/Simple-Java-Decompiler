20e5bb5f7e12bb48c6a2a001e6203c1dbe33da57
harpend <jackptvideo@gmail.com>
2024-12-18 22:40:47 +0000

    reduce if-style statements

--- parser/cfg/CFGReducer.java
+++ parser/cfg/CFGReducer.java
@@ -7,58 +7,80 @@ public class CFGReducer {
     public static boolean reduceCFG(ControlFlowGraph cfg) {
         BasicBlock head = cfg.head;
         HashSet<BasicBlock> visited = new HashSet(cfg.bbList.size());
-        return reduceCFG(head, visited);
-    }
-
-    public static boolean reduceCFG(BasicBlock bb, HashSet<BasicBlock> visited) {
-        if (!matchType(bb, BasicBlock.GROUP_END) && (!visited.contains(bb))) {
-            visited.add(bb);
-            // use match type instead
-            switch (bb.TYPE) {
-                case BasicBlock.TYPE_STATEMENTS:
-                case BasicBlock.TYPE_IF:
-                case BasicBlock.TYPE_IF_ELSE:
-                    return reduceCFG(bb.next, visited);
-                case BasicBlock.TYPE_CONDITIONAL_BRANCH:
-                    return reduceCB(bb, visited);
-                case BasicBlock.TYPE_LOOP:
-                    // reduce nodes in loop
-                    return reduceLoop(bb, visited);
-                default:
-                    throw new AssertionError();
+        for (BasicBlock bb : cfg.bbListPostorder) {
+            if (bb.successors.size() == 2) {
+                if (bb.matchType(BasicBlock.TYPE_CONDITIONAL_BRANCH)) {
+                    reduceConditional(bb, cfg);
+                } else {
+                    System.out.println("non conditional branch with 2 successors");
+                    System.exit(1);
+                }
             }
         }
+
         return true;
     }
 
-    private static boolean reduceCB(BasicBlock bb, HashSet<BasicBlock> visited) {
-        if (reduceCFG(bb.next, visited) && reduceCFG(bb.branch, visited)) {
-            return reduceCB(bb);
+    private static void reduceConditional(BasicBlock bb, ControlFlowGraph cfg) {
+        if (bb.branch.successors.contains(bb.next)) {
+            // normal if
+            BasicBlock ifBB = cfg.newTypeBB(BasicBlock.TYPE_IF);
+            for (BasicBlock pred : bb.predecessors) {
+                ifBB.predecessors.add(pred);
+                pred.successors.add(ifBB);
+                pred.successors.remove(bb);
+            }
+
+            for (BasicBlock succ : bb.next.successors) {
+                ifBB.successors.add(succ);
+                succ.predecessors.add(ifBB);
+                succ.predecessors.remove(bb);
+            }
+
+            ifBB.subNodes.add(bb); ifBB.subNodes.add(bb.branch); ifBB.subNodes.add(bb.next);
+            bb.predecessors.clear();
+            bb.next.successors.clear();
+            int index = cfg.bbListPostorder.indexOf(bb);
+            cfg.bbListPostorder.set(index, ifBB);
+            cfg.bbListPostorder.removeAll(ifBB.subNodes);
+        } else {
+            // if-else
+            if (bb.next.successors.size() > 1 || bb.branch.successors.size() > 1) {
+                System.out.println("too many successors for branch or next");
+                System.exit(1);
+            } else if (bb.next.successors.size() == 1 && bb.branch.successors.size() == 1) {
+                if (!bb.next.successors.containsAll(bb.branch.successors)) {
+                    System.out.println("unexpected structure for if-else");
+                    System.exit(1);
+                }
+            }
+
+            BasicBlock ifeBB = cfg.newTypeBB(BasicBlock.TYPE_IF_ELSE);
+            for (BasicBlock pred : bb.predecessors) {
+                ifeBB.predecessors.add(pred);
+                pred.successors.add(ifeBB);
+                pred.successors.remove(bb);
+            }
+
+            for (BasicBlock succ : bb.next.successors) {
+                ifeBB.successors.add(succ);
+                succ.predecessors.add(ifeBB);
+                succ.predecessors.remove(bb);
+            }
+
+            for (BasicBlock succ : bb.branch.successors) {
+                ifeBB.successors.add(succ);
+                succ.predecessors.add(ifeBB);
+                succ.predecessors.remove(bb);
+            }
+
+            ifeBB.subNodes.add(bb); ifeBB.subNodes.add(bb.branch); ifeBB.subNodes.add(bb.next);
+            bb.predecessors.clear();
+            bb.next.successors.clear();
+            bb.branch.successors.clear();
+            int index = cfg.bbListPostorder.indexOf(bb);
+            cfg.bbListPostorder.set(index, ifeBB);
+            cfg.bbListPostorder.removeAll(ifeBB.subNodes);
         }
-        
-        return false;
-    }
-
-    private static boolean reduceCB(BasicBlock bb) {
-        if (bb.next.matchType(BasicBlock.GROUP_END) && (bb.next.predecessors.size() <= 1)) {
-            // createIf();
-            bb.next.stringify();
-            System.exit(0);
-            return true;
-        }
-        
-        System.exit(1);
-        
-
-        return false;
-    }
-
-    private static boolean reduceLoop(BasicBlock bb, HashSet<BasicBlock> visited) {
-        // reduce nodes in loop
-        return true;
-    }
-
-    private static boolean matchType(BasicBlock bb, int type) {
-        return (bb.TYPE & type) != 0;
     }
 }
